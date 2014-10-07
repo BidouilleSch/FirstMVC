7ac2ffa654ab9e4e95aa578d9365db6717465d8c
BidouilleSch <Dominique.Schillings@gmail.com>
2014-10-07 16:47:37 +0200

    Fin cours 7/10 encore à compléter Merci

---
+++ FirstMVC/Controllers/ReponseController.cs
@@ -0,0 +1,24 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class ReponseController : Controller
+    {
+        [HttpGet]
+        public ViewResult ConfirmerForm()
+
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ConfirmerForm(GuestResponse guestResponse)
+        { return View("Merci, guestResponse");
+        }
+	}
+}
---
+++ FirstMVC/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC.Models
+{
+    public class GuestResponse
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set;}
+        public string Email { get; set;}
+        public bool? presence {get;set;}
+    }
+}
